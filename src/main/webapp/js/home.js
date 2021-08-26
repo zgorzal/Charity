15ffbc89008b5558e33925b452f6c7a3b4765d1c
zgorzal <zgorzalek.piotr@gmail.com>
2021-08-26 19:09:48 +0200

    Add Institutionlist on view HTML FE

---
+++ src/main/webapp/js/home.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const institutionsList = document.querySelector(".help--slides-items")
+
+    function apiListInstitution() {
+        return fetch(
+            'http://localhost:8080/institution',
+        ).then(resp => {
+            return resp.json();
+        });
+    }
+
+    apiListInstitution().then(result => {
+            let count = 0;
+            result.forEach(function (el) {
+                count++;
+                const divCol = document.createElement("div");
+                divCol.classList.add("col");
+                const divTitle = document.createElement("div");
+                divTitle.innerText = el.name;
+                divTitle.classList.add("title");
+                const divSubtitle = document.createElement("div");
+                divSubtitle.innerText = el.description;
+                divSubtitle.classList.add("subtitle");
+                divCol.appendChild(divTitle);
+                divCol.appendChild(divSubtitle);
+                if (count % 2 === 1) {
+                    const newLi = document.createElement("li");
+                    newLi.appendChild(divCol);
+                    institutionsList.appendChild(newLi);
+                } else {
+                    const oldLi = institutionsList.lastElementChild;
+                    oldLi.appendChild(divCol);
+                    institutionsList.appendChild(oldLi);
+                }
+            });
+        }
+    );
+
+});
