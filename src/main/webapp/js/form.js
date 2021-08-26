fdcc36c5ad2a488e2422ee6e1f8d98ad61efc5dd
zgorzal <zgorzalek.piotr@gmail.com>
2021-08-26 21:34:57 +0200

    Add institution rest in form

--- src/main/webapp/js/form.js
+++ src/main/webapp/js/form.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const divStepOne = document.getElementById("stepOne");
+    const divStepthree = document.getElementById("stepThree");
 
     function apiListCategories() {
         return fetch(
@@ -28,6 +29,46 @@ document.addEventListener("DOMContentLoaded", function () {
                 label.appendChild(description);
                 div.appendChild(label);
                 divStepOne.insertBefore(div, divStepOne.lastElementChild);
+            });
+        }
+    );
+
+    function apiListInstitution() {
+        return fetch(
+            'http://localhost:8080/institution',
+        ).then(resp => {
+            return resp.json();
+        });
+    }
+
+    apiListInstitution().then(result => {
+            result.forEach(function (el) {
+                const div = document.createElement("div");
+                div.classList.add("form-group");
+                div.classList.add("form-group--checkbox");
+                const label = document.createElement("label");
+                const input = document.createElement("input");
+                input.type = "radio";
+                input.name = "organization";
+                input.value = "old";
+                const radio = document.createElement("span");
+                radio.classList.add("checkbox");
+                radio.classList.add("radio");
+                const description = document.createElement("span");
+                description.classList.add("description");
+                const title = document.createElement("div");
+                title.classList.add("title")
+                title.innerText = el.name;
+                const subtitle = document.createElement("div");
+                subtitle.classList.add("subtitle");
+                subtitle.innerText = el.description;
+                label.appendChild(input);
+                label.appendChild(radio);
+                description.appendChild(title);
+                description.appendChild(subtitle);
+                label.appendChild(description);
+                div.appendChild(label);
+                divStepthree.insertBefore(div, divStepthree.lastElementChild);
             });
         }
     );
